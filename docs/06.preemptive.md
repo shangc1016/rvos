f122515734f95978bd9f4647cb57fbe0f58289d3
shang <56389636+shangc1016@users.noreply.github.com>
2023-11-14 11:47:28 +0800

    add swtimer support.

---
+++ docs/06.preemptive.md
@@ -0,0 +1,8 @@
+### 抢占式多任务
+
+
+```
+ret指令：函数返回指令，ra -> pc
+mret指令：machine状态的return指令，mepc -> pc
+# mepc 的意思是machine exception pc.
+```
---
+++ src/user.c
@@ -0,0 +1,63 @@
+#include "os.h"
+
+#define DELAY 100
+
+// caused exception
+void trap_maker() {
+    *((int*)(0x0)) = 123;
+
+    printf("back from trap.\n");
+}
+
+
+void func(void *args) {
+	printf("swtimer call this from kernel, id = %d\n", *(int*)args);
+}
+
+static void user_func0() {
+
+
+	int id = 0;
+	swtimer_create(func, &id, 3, 1);
+
+	while(1) {
+		// spinlock_lock();
+		uart_puts("Task 0: Created!\n");
+		// yield();
+		for (int i = 0; i < 30; i++) {
+			printf("Task 0: Running...\n");
+
+
+			task_delay(DELAY);
+		}
+		// spinlock_unlock();
+	}
+}
+
+static void user_func1() {
+    uart_puts("Task 1: Created!\n");
+
+	int id = 1;
+	swtimer_create(func, &id, 7, 1);
+	
+	while (1) {
+		uart_puts("Task 1: Running...\n");
+		task_delay(DELAY);
+	}
+}
+
+static void user_func2() {
+	printf("Task 2: Crated!\n");
+	while(1) {
+		printf("Task 2: Running ... \n");
+		task_delay(DELAY);
+	}
+}
+
+// add time-sharing proc here.
+void user_main() {
+	// create two proc.
+    create_proc(user_func0);
+    create_proc(user_func1);
+	create_proc(user_func2);
+}
